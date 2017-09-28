114fb43d0ff7a5107b103d23d79060bdb6ddb75d
KonstantinHronus <ursa.le1@yandex.ru>
2017-09-28 22:54:36 +0700

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Константин on 27.09.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        dogs igor = new dogs();
+        igor.age = 7;
+        igor.familyname = "Svyatogorov";
+        igor.longofdick = 17;
+        igor.name = "Igor";
+        igor.spied = 18;
+        igor.weiht = 10;
+       igor.gav();
+    }
+}
---
+++ dogs.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Константин on 27.09.2017.
+ */
+public class dogs {
+
+    public String name = "Rodger";
+    public String familyname = "Braun";
+    public int weiht = 10;
+    public int age = 5;
+    public int longofdick = 20*age/weiht;
+    public int spied = 14*longofdick;
+
+    public void gav(){
+        System.out.println("Gav! Gav! Go away! I'm " + name + " " + familyname + ". My weiht is " + weiht + ". And i'm " + age + ". My spied is " + spied + " and larch of dick " + longofdick + "." );
+    }
+    public void kusaet () {
+
+    }
+    public void HideDick () {
+
+    }
+    public void DeployDick () {
+
+    }
+}
